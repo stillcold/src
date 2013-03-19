1a685cdaf12ce45adfc33b2188bacf52f40097cf
Still.Cold <mr_kernel@163.com>
2013-03-19 22:57:37 +0800

    source: Update the code to test point in C.

--- c/0150-test-point.c
+++ c/0150-test-point.c
@@ -1,7 +1,15 @@
 #include "stdio.h"
-
-void swap1(int* a, int* b);
-void swap2(int *a, int* b);
+/*
+ * Both swap1 and swap2 are OK.
+ * It is hard to believe, check the postion of "*" in both function.
+ * And also notice that swap2 has no return statement.
+ * One more thing is:
+ * 		int *a = 5; -- this is illegal.
+ * 			if this can work, then the value can be exchanged easily, because it is
+ *			just like an array.
+ */
+void swap1(int* a, int *b);
+void swap2(int *a, int * b);
 
 void swap1(int* a, int* b){
 	int c;
@@ -13,16 +21,19 @@ void swap1(int* a, int* b){
 
 void swap2(int *a, int *b){
 	int temp;
-	temp = a;
-	a = b;
-	b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 int main(void)
 {
-	int* a = 5;
-	int* b = 6;
-	swap1(a,b);
-	printf("%d , %d", *a, *b);
+	int a = 5;
+	int b = 6;
+	printf("The original value:\n a = %d, b = %d\n", a, b);
+	swap1(&a, &b);
+	printf("%d, %d\n", a, b);
+	swap2(&a, &b);
+	printf("%d, %d\n", a, b);
 	return 0;
 }
