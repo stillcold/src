b97823493da5767f74cce8440540f9f892b635da
Still.Cold <mr_kernel@163.com>
2013-05-30 18:07:01 +0800

    source: Update structure head file.

--- c/include/mystructures.h
+++ c/include/mystructures.h
@@ -4,6 +4,8 @@
 #define _MYSTRUCTRURE_H
 #endif
 
+typedef u32 elementtype;
+
 /* Node of a linked-list */
 
 typedef struct node
@@ -12,4 +14,22 @@ typedef struct node
     struct node *next;
 } node;
 
+/*
+ * Implementation of the funcstions below can be found in 0260
+ *
+ * Watch out this wrong usage:
+ *
+ *     boolean initiateList(node *head){node *new_node = ...; head = new_node}
+ *
+ *     then we invoke it like this:
+ *
+ *         initiateList(head) 
+ *
+ *     it can't initiate the head, because the head is in fact not changed by
+ *     the function. Most function can only chang *head, can't change head 
+ *     itself. But initiateList(head) try to change head itself, it can't work.
+ */
+node* initiateList(void);
 
+/* Build a sorted list */
+boolean insertElement(node *head, elementtype element);
