bf1a9c96922c37f7d64094b21ea8147778fbac3a
Still.Cold <mr_kernel@163.com>
2013-09-29 21:28:33 +0800

    source: Update Z1099

--- c/upper_layer/Z1099.c
+++ c/upper_layer/Z1099.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 /*
  * still cannot pass!
  */
 int main(){
 	int n=0;
-	char *cha;
-	do{
-		scanf("%s",cha);
+	char cha[82];
+	while(scanf("%s",cha) != EOF){
 		if (strcmp(cha,"<br>")==0) {
 			n=0;
 			printf("\n");
 		} else if (strcmp(cha,"<hr>")==0) {
 			if (n==0) {
 				printf("--------------------------------------------------------------------------------\n");
+                n = 0;
 			} else {
 				printf("\n--------------------------------------------------------------------------------\n");
+                n = 0;
 			}
 		} else if ((n+=strlen(cha))<80){
 			printf("%s ",cha);
@@ -25,9 +27,9 @@ int main(){
 				n=0;
 			} else {
 				printf("\n%s ",cha);
-                                n=0;
+                n=0;
 			}
 		}
-	}while(cha!=NULL&&cha!=EOF);
+	}
 	return 0;
 }
