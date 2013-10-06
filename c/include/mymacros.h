947c04b4e15d316c0625ef6e82d85718e8a14e1e
Still.Cold <mr_kernel@163.com>
2013-10-06 19:19:50 +0800

    source: Add some macros in head file

--- c/include/mymacros.h
+++ c/include/mymacros.h
@@ -1,6 +1,9 @@
 #ifndef _MYMACROS_H
 #define _MYMACROS_H
 
+#define ONE_THOUSAND        1000
+
+#define max(x,y)            ((x)>(y)?(x):(y))
 #define min(x,y)            ((x)<(y)?(x):(y))
 
 #endif
