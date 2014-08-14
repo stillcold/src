bbd932074c46cccfe55afde1122b4ede07a64471
Chao.guan <mr_kernel@163.com>
2014-08-14 20:04:26 +0800

    source: Add new C sharp project SendMail

---
+++ c_sharp/SendMail/SendMail/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = SendMail.Send();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
