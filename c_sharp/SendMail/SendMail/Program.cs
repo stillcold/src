76ade8f7ad5e8de04089240c441687beeb99c44b
Chao.guan <mr_kernel@163.com>
2015-02-06 15:22:36 +0800

    source: Add a useless comment.

--- c_sharp/SendMail/SendMail/Program.cs
+++ c_sharp/SendMail/SendMail/Program.cs
@@ -6,7 +6,9 @@ namespace SendMail
     {
         static void Main(string[] args)
         {
-            string result = SendMail.Send();
+            string result = SendMail.Send("mr_kernel@163.com",
+                          "chao.guan", "773678279@qq.com", "主题",
+                          "主体", "mr_kernel", "m@902117jd35zln", "smtp.163.com", "");
             Console.WriteLine(result);
             Console.ReadKey();
         }
--- python/helloworld.py
+++ python/helloworld.py
@@ -2,4 +2,8 @@
 #filename: helloworld.py
 #run it in this way: python helloworld.py
 #Lst edit by Li
+import sys
 print 'Hello World!'
+for line in sys.stdin:
+	a = line.split()
+	print a[0]
